231beb26e2e32182d8da489e5ba313c6f541895a
Saras <saras.me108@gmail.com>
2022-01-14 21:44:35 +0545

    Final Touch

--- main.py
+++ main.py
@@ -28,44 +28,39 @@ if __name__ == "__main__":
     
     loss_fn = MSE()
 
+    #Using Swish Activation Function
     z1 = dense.forward(x)
     sig1 = sigmoid.forward(z1)
     z2 = dense2.forward(sig1)
     y_pred = activation2.forward(z2)
 
+    sigmoid_loss = loss_fn.loss(y_true, y_pred)
+    print("sigmoid loss: ", sigmoid_loss)
+    print("sigmoid loss's mean: ",np.mean(sigmoid_loss))
+
+    dldy_pred = loss_fn.gradient(y_true , y_pred)
+    # print("lldy: ",dldy_pred)
+    dldz2 = activation2.backward(dldy_pred)
+    # print("dldz2: ",dldz2)
+    dLda1 = dense2.backward(dldz2)
+    # print("dLda1: ",dLda1)
+    dLz1 = sigmoid.backward(dLda1)
+    dLdw = dense.backward(dLz1)
+    print("updated weight using Sigmoid Activation Function:", W1-0.01*dLdw)
+
+    
+    #Using Swish Activation Function
     sw1 = swish1.forward(z1)
     sw2 = dense2.forward(sw1)
     y_pre = swish2.forward(sw2)
 
-    # loss = loss_fn.loss(y_true, y_pred)
-    # print("loss: ", loss)
-    # print("loss's mean: ",np.mean(loss))
-
-    # sigloss = loss_fn.loss(y_true, y_pre)
-    # print("SIG loss: ", sigloss)
-    # print("sig loss's mean: ",np.mean(sigloss))
-
-    # dldy_pred = loss_fn.gradient(y_true , y_pred)
-    # print("lldy: ",dldy_pred)
-    # dldz2 = activation2.backward(dldy_pred)
-    # print("dldz2: ",dldz2)
-    # dLda1 = dense2.backward(dldz2)
-    # print("dLda1: ",dLda1)
-    # dLz1 = sigmoid.backward(dLda1)
-    # dLdw = dense.backward(dLz1)
-    
+    swishloss = loss_fn.loss(y_true, y_pre)
+    print("swish loss: ", swishloss)
+    print("swish loss's mean: ",np.mean(swishloss))
 
     d1 =loss_fn.gradient(y_true , y_pre)
-    # a = swish2.gradient(d1)
-    # print(d1)
     d2 = swish2.backward(d1)
     d3 = dense2.backward(d2)
     d4 = swish1.backward(d3)
     d5 = dense.backward(d4)
-    print(d2)
-
-
-
-
-
-    
+    print("updated weight using Swish Activation Function :", W1-0.01*d5)
